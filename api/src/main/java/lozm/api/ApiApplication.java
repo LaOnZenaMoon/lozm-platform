ea515eb1c9a0743f9fcf374a0701dee8e9da68fc
laonzenamoon@gmail.com <>
2020-03-21 00:51:46 +0900

    - re-setting module

---
+++ api/src/main/java/lozm/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package lozm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}

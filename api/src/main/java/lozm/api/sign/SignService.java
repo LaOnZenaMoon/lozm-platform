c076b8e967bd58c9afd39446501c7fb1b866a211
laonzenamoon@gmail.com <>
2020-04-11 16:04:14 +0900

    - User sign in&out 기능 개발중...

--- api/src/main/java/lozm/api/sign/SignService.java
+++ api/src/main/java/lozm/api/sign/SignService.java
@@ -1,17 +1,25 @@
 package lozm.api.sign;
 
 import lombok.RequiredArgsConstructor;
+import lozm.core.exception.APIException;
 import lozm.core.vo.sign.SignVo;
+import lozm.domain.entity.user.User;
+import lozm.domain.repository.user.UserRepositorySupport;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SignService {
+    
+    private final UserRepositorySupport userRepositorySupport;
 
-    public void signIn(SignVo signVo) {
-
+    public void signIn(SignVo signVo) throws Exception {
+        List<User> userList = userRepositorySupport.selectUserDetail(signVo);
+        if(userList.size() < 1) new APIException("USER_0002", "User doesn't exist.");
     }
 
 }
--- domain/src/main/java/lozm/domain/repository/user/UserRepositorySupport.java
+++ domain/src/main/java/lozm/domain/repository/user/UserRepositorySupport.java
@@ -1,9 +1,16 @@
 package lozm.domain.repository.user;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lozm.core.vo.sign.SignVo;
 import lozm.domain.entity.user.User;
+import org.graalvm.compiler.replacements.nodes.ArrayEqualsNode;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static lozm.domain.entity.user.QUser.user;
 
 @Repository
 public class UserRepositorySupport extends QuerydslRepositorySupport {
@@ -15,6 +22,17 @@ public class UserRepositorySupport extends QuerydslRepositorySupport {
         this.jpaQueryFactory = jpaQueryFactory;
     }
 
-
+    public List<User> selectUserDetail(SignVo signVo) {
+        return jpaQueryFactory
+                .select(Projections.fields(
+                    User.class
+                ))
+                .from(user)
+                .where(
+                        user.identifier.eq(signVo.getIdentifier())
+                        .and(user.password.eq(signVo.getPassword()))
+                )
+                .fetch();
+    }
 
 }

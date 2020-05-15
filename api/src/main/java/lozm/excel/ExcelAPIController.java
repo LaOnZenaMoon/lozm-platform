8526300848574b5c9fff2b5202018214ee7d0e2e
laonzenamoon@gmail.com <>
2020-05-15 13:03:47 +0900

    - download the excel 개발중... => sample 다운로드 개발

--- api/src/main/java/lozm/excel/ExcelAPIController.java
+++ api/src/main/java/lozm/excel/ExcelAPIController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RequestMapping(value = "/api/excel")
@@ -15,8 +16,8 @@ public class ExcelAPIController {
 
 
     @GetMapping(value = "/download", produces = "application/vnd.ms-excel")
-    public String downloadExcel() {
-        return "excelView";
+    public ModelAndView downloadExcel() {
+        return new ModelAndView("excelView");
     }
 
 }

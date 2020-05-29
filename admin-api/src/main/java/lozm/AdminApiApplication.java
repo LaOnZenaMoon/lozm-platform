c66a2acc59331dc4cdbbee5fd42a3196a203f12e
laonzenamoon@gmail.com <>
2020-05-29 22:25:31 +0900

    - 기타 리팩토링

---
+++ admin-api/src/main/java/lozm/AdminApiApplication.java
@@ -0,0 +1,14 @@
+package lozm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableJpaAuditing
+public class AdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApiApplication.class, args);
+    }
+
+}

28cf5c2e408cd122aac9a850577a4205abee9f85
laonzenamoon@gmail.com <>
2020-07-09 11:22:18 +0900

    - chatting-api 모듈 개발중... => client 에서 보낸 메시지가 server 에 매핑이 안 되던 이슈 해결.

--- chatting-api/src/main/java/lozm/api/chatting/controller/WebSocketAPIController.java
+++ chatting-api/src/main/java/lozm/api/chatting/controller/WebSocketAPIController.java
@@ -4,7 +4,6 @@ package lozm.api.chatting.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,12 +19,6 @@ public class WebSocketAPIController {
     public void sendMessageToTopic(MessageDto messageDto) {
         log.debug("Writer: " + messageDto.getWriter() +", Message: "+messageDto.getMessage());
 		template.convertAndSend("/topic/"+messageDto.getTopicId(), messageDto);
-    }
-
-    @MessageMapping
-    public String getWebSocketBroadcast(String test) {
-        log.debug(test);
-        return "stomp-broadcast";
     }
 
 }
